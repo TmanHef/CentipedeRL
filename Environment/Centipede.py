187ea9d3be7ac25ecd1c2d7a6104e1b87cff8914
Tommy Hefner <tommy309030@gmail.com>
2018-01-27 16:24:41 +0200

    added functionality to Centipede

--- Environment/Centipede.py
+++ Environment/Centipede.py
@@ -1,32 +1,22 @@
 import numpy as np
 
+
 # a class modelling the game
 # player 0 is assumed to be the one who starts
 class Centipede:
-
     numOfLegs = None
     numOfRounds = None
 
-    # payoff is a 2xNumOfLegs matrix,
+    # payoff is a 2x(NumOfLegs + 1)  matrix (+ 1 in case PASS was the action in the last leg),
     # row 0 represents the payoff of player 0 at each stage
     # symmetrically for row 1
     payoffs = None
-    player0 = None
-    player1 = None
 
-    def __init__(self, legs, rounds, playerA, playerB):
+    def __init__(self, legs, rounds):
         self.numOfLegs = legs
         self.numOfRounds = rounds
 
-        if np.random.random_sample() > 0.5:
-            self.player0 = playerA
-            self.player1 = playerB
-        else:
-            self.player0 = playerB
-            self.player1 = playerA
-
-
-        self.payoffs = np.matrix(np.zeroes((2, legs)))
+        self.payoffs = np.matrix(np.zeros((2, legs + 1)))
         for i in range(1, legs + 1):
             if (i % 2) == 0:
                 self.payoffs[0, i] = 2 ^ (i - 1)
@@ -35,3 +25,17 @@ class Centipede:
                 self.payoffs[1, i] = 2 ^ (i - 1)
                 self.payoffs[0, i] = 2 ^ (i + 1)
 
+    def get_next_state(self, leg, round, action):
+
+        # if TAKE was made or PASS was made in the last leg - return next round state
+        if action == 'TAKE' | leg == self.numOfLegs:
+            return 1, round + 1
+
+        else:
+            return leg + 1, round
+
+    def get_payoff_player0(self, leg):
+        return self.payoffs[0, leg]
+
+    def get_payoff_player1(self, leg):
+        return self.payoffs[1, leg]
